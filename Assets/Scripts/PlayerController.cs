cdf85b98f3ccc9ffa57c25b6dcbbce470adb2b38
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-08-21 15:27:59 +0200

    Make forward movement possible by pressing 'w' key
    https://www.youtube.com/watch?v=zx6K86AycNc

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public const float maxSpeed = 5;
+    private Vector3 _velocity;
+    private CharacterController _controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward)
+                                  * Time.deltaTime * maxSpeed;
+        }
+        // Vector3 movementRatio = new Vector3(Input.GetAxis("Horizontal"), 0,
+        //                                     Input.GetAxis("Vertical"));
+
+        // _controller.Move(movementRatio * Time.deltaTime);
+    }
+}
