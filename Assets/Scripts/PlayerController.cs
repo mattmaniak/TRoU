06d9beee9cd9284f8bd4836489bfa4c497b2c495
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-05 13:01:59 +0200

    Add move to mouse click functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 1.5f;
     public float runSpeed = 2.5f;
     public float jumpHeight = 1.0f;
+    NavMeshAgent playerMeshAgent;
 
     bool _falling = false;
     bool _jumping = false;
@@ -14,14 +16,27 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        playerMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ControlByKeyboard();
-        SimpleJump();
+        MoveToMouseClick();
+    }
+
+    void MoveToMouseClick()
+    {
+        RaycastHit destinationHit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(
+                    Camera.main.ScreenPointToRay(Input.mousePosition),
+                    out destinationHit, 100))
+            {
+                playerMeshAgent.destination = destinationHit.point;
+            }
+        }
     }
 
     void ControlByKeyboard()
