82a6aa4a66bb8cb1b9c5ff4049721b06cad3c62c
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-09-05 12:08:15 +0200

    Implement isometric camera with mouse controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraHolder;
+    public float cameraSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        var mousePositionOnScreen = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        var screenSize = new Vector3(Screen.width, Screen.height, 0.0f);
+        var relativeMousePosition = new Vector3(mousePositionOnScreen.x / screenSize.x,
+                                                mousePositionOnScreen.y / screenSize.y,
+                                                0.0f);
+
+        if (relativeMousePosition.x < 0.25f)
+        {
+            cameraHolder.transform.position += transform.TransformDirection(Vector3.left)
+                                               * Time.deltaTime * cameraSpeed;
+        }
+        else if (relativeMousePosition.x > 0.75f)
+        {
+            cameraHolder.transform.position += transform.TransformDirection(Vector3.right)
+                                               * Time.deltaTime * cameraSpeed;
+        }
+
+        if (relativeMousePosition.y < 0.25f)
+        {
+            cameraHolder.transform.position += transform.TransformDirection(Vector3.down)
+                                               * Time.deltaTime * cameraSpeed;
+        }
+        else if (relativeMousePosition.y > 0.75f)
+        {
+            cameraHolder.transform.position += transform.TransformDirection(Vector3.up)
+                                               * Time.deltaTime * cameraSpeed;
+        }
+        Debug.Log(cameraHolder.transform.position);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,26 +7,24 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 1.5f;
     public float runSpeed = 2.5f;
     public float jumpHeight = 1.0f;
-    public float mouseSensitivity = 1.7f;
-    public Transform camera;
 
-    private bool _falling = false;
-    private bool _jumping = false;
+    bool _falling = false;
+    bool _jumping = false;
     
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
 
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         ControlByKeyboard();
         SimpleJump();
     }
 
-    private void ControlByKeyboard()
+    void ControlByKeyboard()
     {
         float speed = walkSpeed;
 
@@ -39,23 +37,23 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey("w"))
             {
                 transform.position += transform.TransformDirection(Vector3.forward)
-                                    * Time.deltaTime * speed;
+                                      * Time.deltaTime * speed;
             }
             else if (Input.GetKey("s"))
             {
                 transform.position += transform.TransformDirection(Vector3.back)
-                                    * Time.deltaTime * speed;
+                                      * Time.deltaTime * speed;
             }
             
             if (Input.GetKey("a"))
             {
                 transform.position += transform.TransformDirection(Vector3.left)
-                                    * Time.deltaTime * speed;
+                                      * Time.deltaTime * speed;
             }
             else if (Input.GetKey("d"))
             {
                 transform.position += transform.TransformDirection(Vector3.right)
-                                    * Time.deltaTime * speed;
+                                      * Time.deltaTime * speed;
             }
             if (Input.GetKey("space"))
             {
@@ -68,7 +66,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void SimpleJump()
+    void SimpleJump()
     {
         if (_jumping)
         {
@@ -92,22 +90,5 @@ public class PlayerController : MonoBehaviour
                 _jumping = _falling = false;
             }
         }
-    }
-
-    private void RotateCamera()
-    {
-        float horizontalRotation = Input.GetAxis("Mouse X");
-        float verticalRotation = Input.GetAxis("Mouse Y");
-        Vector3 currentRotation = new Vector3(0.0f, 0.0f, 0.0f);
-
-        transform.Rotate(0, horizontalRotation * mouseSensitivity, 0.0f);
-        camera.Rotate(-verticalRotation * mouseSensitivity, 0.0f, 0.0f);
-
-        currentRotation = camera.localEulerAngles;
-        if (currentRotation.x > 180.0f)
-        {
-            currentRotation.x -= 360.0f;
-        }
-        camera.localRotation = Quaternion.Euler(currentRotation);
     }
 }
